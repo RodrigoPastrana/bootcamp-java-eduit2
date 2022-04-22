21ad7b9a3c55fe300ab6cb14260d063d489b82dd
RodrigoPastrana <rodrigopastra@gmail.com>
2022-04-22 17:05:53 -0300

    Practica Semanal semana 3
    -terminado

--- clase2-primer-proyecto/src/ejercicio2/Ejercicio2.java
+++ clase2-primer-proyecto/src/ejercicio2/Ejercicio2.java
@@ -4,96 +4,89 @@ import java.util.Scanner;
 
 public class Ejercicio2 {
 	public static void main(String[] args) {
-
+		
 		Scanner teclado = new Scanner(System.in);
-		
-		float calificaciones[];
-		String alumnos[];
-		int cantidadAlumnos;
-		float[] promedio;
-		
-		System.out.println("ingrese la cantidad de alumnos que hay");
-		cantidadAlumnos = teclado.nextInt();
-		
-		calificaciones = new float[cantidadAlumnos];
-		alumnos = new String[cantidadAlumnos];
-		promedio = new float[cantidadAlumnos];
-		
-		
-		inicializar(calificaciones, alumnos, teclado);
-		promedio(calificaciones, promedio, alumnos);
-		mayor(calificaciones, alumnos);
-		
-	}
-	public static void inicializar(float calificaciones[], String alumnos[], Scanner teclado) 
-	{
-		for (int i=0; i<calificaciones.length; i++)
+			
+			float calificaciones[];
+			String[] alumnos;
+			int cantidadAlumnos;
+			float[] promedio;
+			
+			System.out.println("ingrese la cantidad de alumnos que hay");
+			cantidadAlumnos = teclado.nextInt();
+			
+			calificaciones = new float[cantidadAlumnos];
+			alumnos = new String[cantidadAlumnos];
+			promedio = new float[cantidadAlumnos];
+			
+			
+			inicializar(calificaciones, alumnos, teclado);
+			promedio(calificaciones, promedio, alumnos);//tengo problemas con este.
+			mayor(calificaciones, alumnos);
+			
+		}
+		public static void inicializar(float calificaciones[], String alumnos[], Scanner teclado) 
 		{
-			System.out.println("Nombre del alumnos");
-			alumnos[i] = teclado.next();
-			
-			int numeroExamenes;
-			
-			System.out.println("ingrese la cantidad de examenes");
-			numeroExamenes = teclado.nextInt();
-			for(int j=0; j< numeroExamenes; j++){
-				System.out.println("ingrese notas");
-				calificaciones[i] = teclado.nextFloat();
-			}
-		}
-	}
-	
-	public static void promedio(float calificaciones[], float promedio[], String alumnos[])
-	{
-		float suma = 0;
-		for(int i = 0; i < calificaciones.length; i++) {
-			suma = suma + calificaciones[i];
-			promedio[i] = suma/calificaciones.length;}
-		
-		if(promedio.length > 0) {
-			float mayorPromedio = promedio[0];
-			float menorPromedio = promedio[0];
-			String alumno = null;
-			for (int i = 1; i < promedio.length; i++) {
+			for (int i=0; i<calificaciones.length; i++)
+			{
+				System.out.println("Nombre del alumno");
+				alumnos[i] = teclado.next();
 				
-				if (promedio[i] > mayorPromedio) {
-					mayorPromedio = promedio[i];
-					alumno = alumnos[i];
+				int numeroExamenes;
+				
+				System.out.println("ingrese la cantidad de examenes");
+				numeroExamenes = teclado.nextInt();
+				for(int j=0; j< numeroExamenes; j++){
+					System.out.println("ingrese notas");
+					calificaciones[i] = teclado.nextFloat();
 				}
-				if(promedio[i] < menorPromedio) {
-					menorPromedio = promedio[i];
-					alumno = alumnos[i];
-				}	
 			}
-			System.out.println("el mayor promedio fue" +mayorPromedio + "y fue de " + alumno);
-			System.out.println("el menor promedio fue" +menorPromedio + "y fue de " + alumno);
-			
 		}
 		
-	}
-	
-	public static void mayor (float calificaciones[], String alumnos[])
-	{
-		if(calificaciones.length > 0) {
-			float mayor = calificaciones[0];
-			float menor = calificaciones[0];
-			String alumno = null;
-			for (int i = 1; i < calificaciones.length; i++) {
-				
-				if (calificaciones[i] > mayor) {
-					mayor = calificaciones[i];
-					alumno = alumnos[i];
+		public static void promedio(float calificaciones[], float promedio[], String alumnos[])
+		{
+			float suma = 0;
+			for(int i = 0; i < calificaciones.length; i++) {
+				suma = suma + calificaciones[i];
+				promedio[i] = suma/calificaciones.length;}
+			
+			for(float aux : promedio)
+			
+			if(promedio.length > 0) {
+				for (int i = 1; i < alumnos.length; i++) {
+					
+					if (promedio[i] <= 7) {
+						System.out.println("el promedio del alumno: " + alumnos[i] + " es mayor o igual a 7 por lo que promociona la materia");
+					}else {
+						System.out.println("el promedio del alumno: " + alumnos[i] + " es menor a 7 por lo que recursa la materia");
+						
+					}	
 				}
-				if(calificaciones[i] <= menor) {
-					menor = calificaciones[i];
-					alumno = alumnos[i];
-				}	
+				
 			}
-			System.out.println("la calificacion mas alta fue" +mayor + "y fue de " + alumno);
-			System.out.println("la calificacion mas baja fue" +menor + "y fue de " + alumno);
 			
 		}
+		
+		public static void mayor (float calificaciones[], String alumnos[])
+		{
+			for(float aux : calificaciones )
+			
+			if(calificaciones.length > 0) {
+				float mayor = calificaciones[0];
+				float menor = calificaciones[0];
+				for (int i = 1; i < calificaciones.length; i++) {
+					float calificacionActual = calificaciones[i];
+					if (calificacionActual > calificaciones[i]) {
+						mayor = calificacionActual;
+					}
+					if(calificacionActual <= calificaciones[i]) {
+						menor = calificacionActual;
+					}	
+				System.out.println("la calificacion mas alta fue " + mayor + " y fue de " + alumnos[i]);
+				System.out.println("la calificacion mas baja fue " + menor + " y fue de " + alumnos[i]);
+				}
+				
+			}
+		}
+
 	}
-
-}
-
